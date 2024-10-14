cb07a1a756185fbdeef866ad5de00f8dd104a3df
rferguson22 <119432511+rferguson22@users.noreply.github.com>
2024-10-14 10:39:54 +0100

    fixed cross sec formula issue

--- include/CrossSecFormulae.h
+++ include/CrossSecFormulae.h
@@ -13,9 +13,9 @@ public:
 	BH(std::shared_ptr<cross_section> target):target(target){}
 
 	double c_sec(double a_s, double a_Q2, double a_t, double a_weight, double a_phi, double a_th){
-		double f1p = this->f1(a_t);
-		double f2p = this->f2(a_t);
-		double m = this->mass;		
+		double f1p = target->f1(a_t);
+		double f2p = target->f2(a_t);
+		double m = target->mass;		
 
 		return ttcscrs.Eval_BH(a_s,a_Q2,a_t,a_weight,a_phi,a_th,f1p,f2p,m);
 	}
